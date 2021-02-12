8955b83ec14be70056188af803955323a62315ab
vaibhavs2 <vaibhavk106@gmail.com>
2021-02-12 16:56:50 +0530

    reset

--- App.js
+++ App.js
@@ -6,77 +6,66 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-   View,
-  StyleSheet,
-  Button,
-  StatusBar,
-} from 'react-native';
+import React, {useMemo,useState}  from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import {Colors,} from 'react-native/Libraries/NewAppScreen';
-import Chats from "./screens/chatList";
-import Conversation from "./screens/conversation";
+import ProductList from "./src/screens/productList";
+import ProductDetail from "./src/screens/productDetail";
+import CartItem from './src/screens/cartItem';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {CartContext} from './src/component/cartContext'
 
 
 
-function HomeScreen({ navigation }) {
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-  onPressLearnMore=()=>{
-    console.log("clicking");
-  }
+
+function HomeTabs() {
   return (
-    <View style={styles.body}>
-    <StatusBar barStyle="default" />
-    <Button
-      onPress={() => navigation.navigate('Chats')}
-      title="Add sections"
-      color="#841584"
-      accessibilityLabel="Navigate to Add sections "
-    />
-    <View style={{marginVertical:6}}></View>
-
-      <Button
-        onPress={() => navigation.navigate('Series')}
-        title="Find number"
-        color="#841584"
-        accessibilityLabel="Navigate to Find number from series"
-        />
-      </View>
+    <Tab.Navigator tabBarOptions={{
+      labelStyle:{
+        fontSize:18,
+        marginBottom:12
+      }
+  }}>
+      <Tab.Screen name="Product" component={ProductList}  options={{ tabBarLabel: "Products" }} />
+      <Tab.Screen name="Cart" component={CartItem}  options={{ tabBarLabel: "Cart"}} />
+    </Tab.Navigator>
+    
   );
 }
 
 
-const Stack = createStackNavigator();
-
 const App: () => React$Node = () => {
 
+  const [getCartItem, setCartItem] = useState([])
+
+
+  const contexValue = useMemo(
+    () => ({
+      addToCart: async (itemList) => {
+        setCartItem(itemList)
+      },
+      cartItem:getCartItem
+    }),
+    []
+  );
+
+
+
   return (
+<CartContext.Provider value={contexValue}>
     <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen name="Chats" component={Chats}  options={{ title: 'Chats' }} />
-    <Stack.Screen name="Conversation" component={Conversation}  options={{ title: "chat" }}/>
-    <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Product" component={HomeTabs}  options={{ title: 'Products' }} />
+        <Stack.Screen name="Detail" component={ProductDetail}  options={{ title: "Detail" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+</CartContext.Provider>
   );
 };
 
 
-
-const styles = StyleSheet.create({
-   
-  body: {
-    backgroundColor: Colors.white,
-    flex:1,
-    justifyContent:'center',
-    padding:8
-  },
-  buttons_:{
-    margin:5
-  }
-  
-});
 
 export default App;
